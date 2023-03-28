bd833d6dfd7c0904ea2d110706b6666cc8f16b28
Mariana Almeida <39881974+mlfa03@users.noreply.github.com>
2023-03-28 05:52:25 -0300

    Create day4_corr_reg_lines.py

---
+++ ML_and_AI/day4_corr_reg_lines.py
@@ -0,0 +1,11 @@
+# Day 4 - Correlation and Regression lines
+
+import math
+import statistics as st 
+
+#4x - 5y +33 = 0   --> valx = (5y - 33)/4
+#20x -9y - 107 = 0
+
+y = 7 
+valx = (107 + 9*y)/20
+print(round(valx, 1))
