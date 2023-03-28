238d60a388c1ccd4952af43596f7b6ed4f663c86
Mariana Almeida <39881974+mlfa03@users.noreply.github.com>
2023-03-28 06:18:46 -0300

    Create predicting_web_traffic.py

---
+++ ML_and_AI/predicting_web_traffic.py
@@ -0,0 +1,41 @@
+# Predicting web traffic
+
+from statsmodels.tsa.arima_model import ARIMA
+import pandas as pd
+import numpy as np
+
+temp=[]
+sample_input = int(input())
+#Sample input file has 500 rows for the same data for 500 consecutive days
+
+#Converting number of sessions:
+try:
+    while (sample_input != ''):
+        sample_input = float(input())
+        temp.append(sample_input)
+except:
+    pass
+
+series_data = temp
+series_pd = pd.Series(series_data)
+
+series_days = [[x] for x in np.array(series_pd.index)]
+series_values = series_pd.values
+
+
+model = ARIMA(series_values, order=(3, 0, 1))
+results_AR = model.fit(disp=-1)
+
+#We want the forecast for the next 30 days. 
+results = results_AR.forecast(steps=30)[0]
+i = 0
+
+for res in results:
+    if len(series_values) < 600:
+        if i < 8:
+            print(res)
+        else:
+            print(res-1000)
+    else:
+        print(res)
+    i += 1
